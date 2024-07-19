7c3e749649379a5806e72d921280022fc090df83
josephl17 <joseph.lauria17@gmail.com>
2024-07-19 16:39:15 -0500

    new link

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,7 +3,7 @@ export default [
         title: 'To-do List',
         imgUrl: './assets/To-do-list.png',
         stack: ['React', 'JavaScript', 'BootStrap', 'CSS'],
-        link: ''
+        link: 'https://josephstodoapp.onrender.com/'
     },
     {
         title: 'Developer Portfolio',
