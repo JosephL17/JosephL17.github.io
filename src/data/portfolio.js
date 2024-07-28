a9ec8199bdb5966064eb10b72fa7382c724ffba7
josephl17 <joseph.lauria17@gmail.com>
2024-07-28 15:39:03 -0500

    update projects

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -6,8 +6,14 @@ export default [
         link: 'https://josephstodoapp.onrender.com/'
     },
     {
+        title: 'Chatbot',
+        imgUrl: './assets/Chatbot_project.png',
+        stack: ['Django', 'Python', 'React', 'TailwindCSS', 'Llama3'],
+        link: 'https://github.com/JosephL17/Chat_bot'
+    },
+    {
         title: 'Developer Portfolio',
-        imgUrl: '',
+        imgUrl: './assets/Portfolio.PNG',
         stack: ['React', 'Tailwind', 'Vite', 'CSS'],
         link: ''
     },
