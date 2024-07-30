2c2265ed675ff9eb60e0325877237a7949713fb6
josephl17 <joseph.lauria17@gmail.com>
2024-07-30 12:36:26 -0500

    updated timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -2,9 +2,15 @@ export default [
 
     {
         year: 'Present',
-        title: 'Student',
+        title: 'Code Platoon | Transitioning Service Member',
+        duration: '15 Weeks',
+        details: 'As part of a rigorous 15-week bootcamp, I am honing my skills as a fullstack software engineer. This immersive program covers a comprehensive curriculum, including JavaScript, Python, React, Django, and PostgreSQL. Through hands-on projects and collaborative coding exercises, I am gaining practical experience in building dynamic web applications and mastering the development lifecycle. This bootcamp not only enhances my technical proficiency but also fosters problem-solving abilities and teamwork, preparing me for a successful career in software development.'
+    },
+    {
+        year: '2024',
+        title: "Associate's Degree in Computer Science",
         duration: '1 year',
-        details: 'Started an Associate of Science in Computer Science at SNHU. Also, as part of a rigorous 15-week bootcamp, I am honing my skills as a fullstack software engineer. This immersive program covers a comprehensive curriculum, including JavaScript, Python, React, Django, and PostgreSQL. Through hands-on projects and collaborative coding exercises, I am gaining practical experience in building dynamic web applications and mastering the development lifecycle. This bootcamp not only enhances my technical proficiency but also fosters problem-solving abilities and teamwork, preparing me for a successful career in software development.'
+        details: 'Gained a solid foundation in software engineering principles, including systematic development approaches, user-centric design, and secure coding practices. Acquired essential skills in software design, development, and testing with a focus on user interface (UI) and user experience (UX). Prepared for a rapidly growing field with a strong emphasis on practical and theoretical aspects of software engineering.'
     },
     {
         year: 'Present',
@@ -13,7 +19,7 @@ export default [
         details: 'Served as a medic for the 82nd Airborne. Supervised all activities of field, clinic, and mobile treatment facilities in support of an Airborne Reconnaissance organization meant to deploy worldwide within 18 hours’ notice. '
     },
     {
-        year: '2014-2017',
+        year: '2017',
         title: 'Personal Trainer',
         duration: '3 years',
         details: "As a personal trainer, I found profound purpose in empowering others to achieve their fitness goals. Specializing in working with elderly clients, I focused on helping them accomplish everyday tasks with greater ease and confidence. One of my proudest moments was when Margret shared that she had walked up the stairs to her apartment pain-free for the first time. Witnessing the transformative impact of our sessions reinforced my commitment to fostering health and well-being in my clients' lives."
