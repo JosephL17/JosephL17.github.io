a7ecbdc72e6a9abfa79a148ecc0806aacb56701d
josephl17 <joseph.lauria17@gmail.com>
2024-09-18 15:29:06 -0500

    fixed contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,18 @@ import React from "react"
 import Title from "./Title"
 
 function Contact() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        alert("Thank you for your message!");
+    };
+
     return (
         <div className="flex flex-col mb-10 mx-auto">
             <div className="flex justify-center items-center">
                 <form 
+                onSubmit={handleSubmit}
                 action="https://getform.io/f/bxojyxla"
                 method="POST"
                 className="flex flex-col w-full md:w-7/12">
@@ -33,7 +41,7 @@ function Contact() {
                     focus:outline-none"
                     />
                     <button 
-                    type='button'
+                    type='submit'
                     className="text-center inline-block px-8 py-3 w-max text-base font-nedium rounded-md text-white bg-slate-500">
                         Work With Me
                     </button>
